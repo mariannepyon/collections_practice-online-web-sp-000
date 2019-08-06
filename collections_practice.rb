af2946e4415b66722188f1f6dec4c90a59c06c8f
Marianne Pyon <mariannepyon@gmail.com>
2019-08-06 19:10:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,7 +18,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  def swap_elements_from_to(array, index, destination_index)
+  def swap_elements_from_to(array, array[i], array(i + 1))
   array.insert("index", array.delete_at("destination_index"))
   index <=> destination_index
   end
