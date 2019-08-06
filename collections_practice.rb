7aaa51fc6b79f3834c463c2e16fdf8acc3883efb
Marianne Pyon <mariannepyon@gmail.com>
2019-08-06 20:16:45 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,58 @@
 
+def sort_array_asc(array)
+  array.sort do |a,b|
+    a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |b,a|
+    a <=> b
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse do |b,a|
+    a <=> b
+end
+end
+
+def kesha_maker(array)
+  array.collect do |string|
+    string[2] = "$"
+    string
+  end
+end
+
+def find_a(array)
+  array.select do |string|
+    string.chr == "a"
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, num|
+    sum + num
+  end
+end
+
+def add_s(array)
+  array.collect.with_index do |element, index|
+    if index == 1
+      element
+    else
+      element + "s"
+    end
+  end
+end
