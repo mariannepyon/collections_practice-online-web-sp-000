6d29892f6218eb84e76541e604bb19e8cb7c9e70
Marianne Pyon <mariannepyon@gmail.com>
2019-08-06 20:02:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,9 +18,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array.sort do |a,b|
-    array[a,b] <=> array[b,a]
-  end
+  array[1], array[2] = array[2], array[1]
   array
 end
 
